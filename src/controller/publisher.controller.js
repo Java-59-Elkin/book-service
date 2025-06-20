5907a56c1193329f076142b69045c8ac5474fdde
Mikhail <elkinms85@gmail.com>
2025-06-20 23:29:17 +0300

    fix bag in publisher.controller

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -7,8 +7,9 @@ export const findPublishersByAuthor = async (req, res) => {
         if (!authorRecord) {
             return res.status(404). json({error: "Author not found"});
         }
+
         const books = await authorRecord.getBooks();
-        const publisher = books.map(book => book.publisher);
+        const publisher = [...new Set(books.map(book => book.publisher))];
         return res.json(publisher);
     } catch {
         console.error('Error finding books by author', e);
