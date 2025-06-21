f4d8b58b8f04c61fbc0691c2f72df3561db8326d
Mikhail <elkinms85@gmail.com>
2025-06-21 17:54:42 +0300

    add removeAuthor

--- src/controller/author.controller.js
+++ src/controller/author.controller.js
@@ -1,4 +1,5 @@
-import {Book} from "../model/index.js";
+import {Author, Book} from "../model/index.js";
+import {sequelize} from "../config/database.js";
 
 export const findBookAuthors = async (req, res) => {
     try {
@@ -16,6 +17,29 @@ export const findBookAuthors = async (req, res) => {
     } catch (e) {
         console.error('Error finding book authors', e);
         return res.status(500).json({error: 'Failed to find book authors'
+        })
+    }
+}
+
+export const removeAuthor = async (req, res) => {
+    const t = await sequelize.transaction();
+    try {
+        const {author} = req.params;
+        const authorRecord = await Author.findByPk(author, {
+            transaction: t
+        });
+        if(!authorRecord) {
+            return res.status(404).json({error: "Author not found"});
+        }
+        await authorRecord.setBooks([], { transaction: t });
+        await authorRecord.destroy({transaction: t});
+        await t.commit();
+        return res.json(authorRecord);
+    } catch (e) {
+        await t.rollback();
+        console.error('Error deleting author:', e.message, e.stack);
+        return res.status(500).json({
+            error: 'Failed to delete author'
         })
     }
 }
